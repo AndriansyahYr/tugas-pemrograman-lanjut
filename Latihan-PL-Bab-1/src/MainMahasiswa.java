5f088e1f606b15510854c45bbca2ddca96ba6d69
Rizkyswann <Rizkyswann>
2016-03-01 18:46:13 +0700

    Update Source MainMahasiswa

--- Latihan-PL-Bab-1/src/MainMahasiswa.java
+++ Latihan-PL-Bab-1/src/MainMahasiswa.java
@@ -1,9 +1,13 @@
 public class MainMahasiswa {
     public static void main(String[] args) {
         Mahasiswa Mhs[]=new Mahasiswa[5];
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 5; i++) {
             Mhs[i] = new Mahasiswa();
             Mhs[i].isiData();            
+        }
+        for (int j = 0; j < 5; j++) {
+            System.out.println("Mahasiswa "+(j+1));
+            Mhs[j].output();
         }
     }
 }
