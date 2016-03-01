8b248b6e5b7b3b98fd222fffe7733fe8ce26f3f0
Dian Aslama <dianaslama6697@gmail.com>
2016-03-01 22:02:10 +0700

    Nyicil method inputan

--- Latihan-PL-Bab-1/src/Mahasiswa/Mahasiswa.java
+++ Latihan-PL-Bab-1/src/Mahasiswa/Mahasiswa.java
@@ -1,13 +1,24 @@
 package mahasiswa;
+import java.util.Scanner;
 public class Mahasiswa {
     int nim;
     String namaMhs;
     int jmlhKata;
     
-    void atribut (String namaMhs, int nim, int jlmhKata){
+    void atribut (String namaMhs, int nim, int jmlhKata){
         this.namaMhs = namaMhs;
         this.nim = nim;
-        this.jmlhKata = jlmhKata;
+        this.jmlhKata = jmlhKata;
+    }
+    
+    void inputan(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan Nim         : ");
+        nim = in.nextInt();
+        System.out.print("Masukkan Nama        : ");
+        namaMhs = in.nextLine();
+        System.out.print("Masukkan Jumlah Kata : ");
+        jmlhKata = in.nextInt();
     }
     
     
