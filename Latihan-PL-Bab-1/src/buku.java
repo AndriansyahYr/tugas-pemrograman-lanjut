0631e106d988ae37524ea5fb14b0fb29c200a48e
ade <Darmawan.Ade98@yahoo.com>
2016-03-01 19:51:03 +0700

    buku.java

---
+++ Latihan-PL-Bab-1/src/buku.java
@@ -0,0 +1,9 @@
+public class buku {
+    int nim, kata;
+    String nama;
+    void inputbuku (int nim, int kata, String nama){
+        this.nim = nim;
+        this.kata = kata;
+        this.nama = nama;
+    }
+}
