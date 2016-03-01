bd08086e9139f5a42dce8f8878c8980ec3c3892b
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-01 21:13:29 +0700

    bikin atribut dan method

--- Latihan-PL-Bab-1/src/CerpenMahasiswa.java
+++ Latihan-PL-Bab-1/src/CerpenMahasiswa.java
@@ -5,5 +5,19 @@ Kelas : TIF - H
 */
 
 public class CerpenMahasiswa{
+	private long nim;
+	private String nama;
+	private int jumlah_kata_hal;
 	
+	public void InputData(String nama, long nim, int jumlah_kata_hal){
+		
+	}
+	
+	public int HitungJumlahKata(int jumlah_kata_hal){
+		return jumlah_kata_hal * 30;
+	}
+	
+	public void showData(){
+
+	}
 }
