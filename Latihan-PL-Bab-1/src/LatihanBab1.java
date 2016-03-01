bd91153d9338fc762516f111c7e970708c76f5da
E.N.D <E.N.D>
2016-03-01 16:56:58 +0700

    Source Code

---
+++ Latihan-PL-Bab-1/src/LatihanBab1.java
@@ -0,0 +1,18 @@
+package latihan.bab1;
+
+import java.util.Scanner;
+
+public class LatihanBab1 {
+    String Nama;
+    Long nim;
+    int jumlahM, kata;
+    int [] data;
+    
+    void inputData(String nama, Long nim, int jumlahM, int kata){
+        Scanner in = new Scanner (System.in);
+        System.out.print("Masukkan Jumlah mahasiswa : ");
+        jumlahM = in.nextInt();
+        int [] data = new int [jumlahM];
+        
+    }
+}
