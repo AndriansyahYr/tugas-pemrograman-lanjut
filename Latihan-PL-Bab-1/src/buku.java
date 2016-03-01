dd365a7f92542a33f1d991d7aadc735ef0a1f486
ade <Darmawan.Ade98@yahoo.com>
2016-03-01 20:32:51 +0700

    buku.java

--- Latihan-PL-Bab-1/src/buku.java
+++ Latihan-PL-Bab-1/src/buku.java
@@ -1,9 +1,14 @@
 public class buku {
     int nim, kata;
     String nama;
-    void inputbuku (int nim, int kata, String nama){
+    void inputBuku(int nim, int kata, String nama) {
         this.nim = nim;
         this.kata = kata;
         this.nama = nama;
+    }
+    void displayBuku() {
+        System.out.println("NIM 	: " + nim);
+        System.out.println("Nama  	: " + nama);
+        System.out.println("Jumlah Kata	: " + kata*30);
     }
 }
