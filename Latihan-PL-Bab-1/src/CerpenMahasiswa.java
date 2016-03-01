1640c8fd82c8eec857993d3ba53e974d52b8240c
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-01 21:34:18 +0700

    update + edit isi method

--- Latihan-PL-Bab-1/src/CerpenMahasiswa.java
+++ Latihan-PL-Bab-1/src/CerpenMahasiswa.java
@@ -10,14 +10,20 @@ public class CerpenMahasiswa{
 	private int jumlah_kata_hal;
 	
 	public void InputData(String nama, long nim, int jumlah_kata_hal){
-		
-	}
-	
-	public int HitungJumlahKata(int jumlah_kata_hal){
-		return jumlah_kata_hal * 30;
+		this.nim = nim;
+		this.nama = nama;
+		this.jumlah_kata_hal = jumlah_kata_hal;
 	}
 	
 	public void showData(){
-
+		System.out.println("Nama		: "+nama);
+		System.out.println("NIM 		: "+nim);
+		if(jumlah_kata_hal>100 && jumlah_kata_hal<500){
+			System.out.println("Jumlah Kata per Halaman : "+jumlah_kata_hal);
+			System.out.println("Jumlah Kata Satu Buku   : "+(jumlah_kata_hal * 30));
+		} else{
+			System.out.println("Jumlah Kata per Halaman : Data yang anda masukkan tidak memenuhi persyaratan! (100 < kata per halaman< 500)");
+			System.out.println("Jumlah Kata 30 Halaman  : Data yang anda masukkan tidak memenuhi persyaratan! (100 < kata per halaman< 500)");
+		}
 	}
 }
