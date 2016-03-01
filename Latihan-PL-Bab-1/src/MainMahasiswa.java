326b4798c6ac3901a13181e4238fb045721147fd
Maya Novita <Maya Novita>
2016-03-02 02:15:49 +0700

    Tugas Update

--- Latihan-PL-Bab-1/src/MainMahasiswa.java
+++ Latihan-PL-Bab-1/src/MainMahasiswa.java
@@ -15,24 +15,25 @@ public class MainMahasiswa {
     public static void main(String[] args) {
         Scanner my = new Scanner(System.in);
         Scanner out = new Scanner(System.in);
-        String nama_mahasiswa;
-        int nim, jumlah_kata_perhalaman;
-        
-        Mahasiswa jumlah_mahasiswa = new Mahasiswa();
-        System.out.print("Jumlah Mahasiswa : \n");
+        System.out.print("Jumlah Mahasiswa : ");
         int jum_mhs = my.nextInt();
+        Mahasiswa jumlah_mahasiswa []= new Mahasiswa[jum_mhs];
+        int nim[]=new int[jum_mhs];
+        String nama_mahasiswa[]= new String [jum_mhs];
+        int jumlah_kata_perhalaman[]=new int [jum_mhs];
 
         for (int i=0; i<jum_mhs; i++) {
-            nim = my.nextInt();
-            nama_mahasiswa = out.nextLine();
-            jumlah_kata_perhalaman = my.nextInt();
-            jumlah_mahasiswa.isiData(nim,nama_mahasiswa,jumlah_kata_perhalaman);
-          
+            nim[i] = my.nextInt();
+            nama_mahasiswa[i] = out.nextLine();
+            jumlah_kata_perhalaman[i] = my.nextInt();
+            jumlah_mahasiswa[i]= new Mahasiswa();
+            jumlah_mahasiswa[i].isiData(nim[i],nama_mahasiswa[i],jumlah_kata_perhalaman[i]);
         }
         System.out.println();
         for (int i = 0; i <jum_mhs; i++) {
-            jumlah_mahasiswa.Tampil();
-            jumlah_mahasiswa.Hitung();
+            jumlah_mahasiswa[i].Tampil();
+            jumlah_mahasiswa[i].Hitung();
         }
+        System.out.println();
     }
 }
