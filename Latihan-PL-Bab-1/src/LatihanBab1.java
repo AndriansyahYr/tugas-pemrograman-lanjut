d738ae237fc5f5eebd254a84f6a129539ee5c99c
E.N.D <E.N.D>
2016-03-01 21:25:38 +0700

    Pembaharuan Identifikasi

--- Latihan-PL-Bab-1/src/LatihanBab1.java
+++ Latihan-PL-Bab-1/src/LatihanBab1.java
@@ -3,37 +3,39 @@ package latihan.bab1;
 import java.util.Scanner;
 
 public class LatihanBab1 {
-    String[] Nama = new String[5], nim =  new String[5];
-    int[] Kata = new int [5];
-    int[] sum = new int [5];
+    int jumlah = 5;
+    
+    String[] nama_mahasiswa = new String[jumlah], nim =  new String[jumlah];
+    int[] jumlah_kata_perhalaman = new int [jumlah];
+    int[] sum = new int [jumlah];
     
     void getinputData(){
         Scanner in = new Scanner (System.in);
         System.out.println("================================================");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlah; i++) {
             System.out.println("Data mahasiswa "+(i+1));
             System.out.print("Nama                      : ");
-            Nama[i] = in.next();
+            nama_mahasiswa[i] = in.next();
             System.out.print("NIM                       : ");
             nim[i] = in.next();
             System.out.print("Jumlah Kata per halaman   : ");
-            Kata[i] = in.nextInt();
+            jumlah_kata_perhalaman[i] = in.nextInt();
         System.out.println("================================================");
         } 
     }
  
     void Tampilkan(){
-        System.out.println("Tampilan AKhir");
+        System.out.println("Tampilan Akhir");
         System.out.println("------------------------------------------------");
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Nama                     : "+Nama[i]);
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Nama                     : "+nama_mahasiswa[i]);
             System.out.println("NIM                      : "+nim[i]);
-            System.out.println("Jumlah Kata per halaman  : "+Kata[i]);
-            if (Kata[i]>100 && Kata[i]<500){
-                sum[i] = Kata[i]*30;
+            System.out.println("Jumlah Kata per halaman  : "+jumlah_kata_perhalaman[i]);
+            if (jumlah_kata_perhalaman[i]>100 && jumlah_kata_perhalaman[i]<500){
+                sum[i] = jumlah_kata_perhalaman[i]*30;
                 System.out.println("Total Kata yang ditulis  : "+sum[i]);
             }
-            else if (Kata[i]<100){
+            else if (jumlah_kata_perhalaman[i]<100){
                 System.out.println("Error, Jumlah Kata anda Kurang dari semestinya");
             }
             else{
