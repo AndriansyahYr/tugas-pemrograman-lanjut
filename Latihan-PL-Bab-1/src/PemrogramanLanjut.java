9c9be21fc2309174409d041641c3cca86567a230
AiKetsuEternalLove <jinxgrimmjow@gmail.com>
2016-03-01 17:49:17 +0700

    T1

---
+++ Latihan-PL-Bab-1/src/PemrogramanLanjut.java
@@ -0,0 +1,13 @@
+package pemrogramanlanjut;
+/*
+ * Author : I Ketut Sidharta Yogatama
+ * NIM    : 155150200111102
+ * Kelas  : IF - H
+ */
+public class PemrogramanLanjut {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Latihan-PL-Bab-1/src/Tugas1.java
@@ -0,0 +1,24 @@
+package pemrogramanlanjut;
+/*
+ * Author : I Ketut Sidharta Yogatama
+ * NIM    : 155150200111102
+ * Kelas  : IF - H
+ */
+public class Tugas1 {
+    private int jumlahKata,hasil;
+    private String namaMahasiswa, NIM;
+    
+    public void setnamaMahasiswa (String M){
+        namaMahasiswa = M;
+    }
+    public void setNIM (String N){
+        NIM = N;
+    }
+    public void setnamaMahasiswa (int K){
+        jumlahKata = K;
+    }
+    public int getKata (){
+        int hasil = jumlahKata*300;
+        return hasil;
+    } 
+}
