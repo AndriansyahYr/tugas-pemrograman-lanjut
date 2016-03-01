dd94c97077d52a86ead6e74b5a205a6e403bb598
Rizkyswann <Rizkyswann>
2016-03-01 18:42:24 +0700

    Update Source Mahasiswa dan Source MainMahasiswa

--- Latihan-PL-Bab-1/src/Mahasiswa.java
+++ Latihan-PL-Bab-1/src/Mahasiswa.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Mahasiswa{
 	String nim;
 	String namaMahasiswa;
-	int jmlKata;
+	int jmlKata, jmlKata2;
 
 	void isiData(){
 		Scanner masukan = new Scanner (System.in);
@@ -17,5 +17,10 @@ public class Mahasiswa{
             System.out.println("Nama :"+namaMahasiswa);
             System.out.println("Nim : "+nim);
             System.out.println("JUmlah Kata : "+jmlKata);
-        }
+        if (jmlKata>100 && jmlKata<500){
+                jmlKata2=jmlKata*30;
+            System.out.println("Jumlah Kata :"+jmlKata2);
+    	} else {System.out.println("Jumlah Kata : tidak memenuhi syarat");}
+    				}
+	}
 }
