23edc296a3e2877c8b0932985bfe36e328f48f0f
arkaan nurul iman <arkaan nurul iman>
2016-03-01 17:33:01 +0700

    membuat class Data

---
+++ Latihan-PL-Bab-1/src/Data.java
@@ -0,0 +1,10 @@
+package tugaspl1;
+
+public class Data {
+    private String[] nama;
+    private int[] nim; int[] kataPerhalaman; int[] jumlahKata;
+    
+    public void setData(String[] nama, int[] nim, int[] kataPerhalaman){
+        
+    }
+}
