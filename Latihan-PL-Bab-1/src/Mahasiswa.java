c9913bbe5bd41321170c3ee1be31d60f193bdc44
Rizkyswann <Rizkyswann>
2016-03-01 17:52:49 +0700

    Update Source class Mahasiswa

--- Latihan-PL-Bab-1/src/Mahasiswa.java
+++ Latihan-PL-Bab-1/src/Mahasiswa.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class Mahasiswa{
-	int jumlahMahasiswa;
 	String nim;
 	String namaMahasiswa;
 	int jmlKata;
 
+	void isiData(){
+		Scanner masukan = new Scanner (System.in);
+		System.out.print("Nama Mahasiswa : ");
+		namaMahasiswa=masukan.nextLine();
+                System.out.print("Nim :");
+		nim=masukan.nextLine();
+                System.out.print("Jumlah Kata : ");
+		jmlKata=masukan.nextInt();
+                }
+        void output(){
+            System.out.println("Nama :"+namaMahasiswa);
+            System.out.println("Nim : "+nim);
+            System.out.println("JUmlah Kata : "+jmlKata);
+        }
 }
