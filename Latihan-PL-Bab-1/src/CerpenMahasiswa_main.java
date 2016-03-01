3772ab9114a5560a1b78fdeae5538a1846ca015e
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-01 21:44:45 +0700

    instansiasi objek, dan inisialisasi variabel

--- Latihan-PL-Bab-1/src/CerpenMahasiswa_main.java
+++ Latihan-PL-Bab-1/src/CerpenMahasiswa_main.java
@@ -6,6 +6,10 @@ Kelas : TIF - H
 
 public class CerpenMahasiswa_main{
 	public static void main(String args[]){
-		
+		Belajar mhs1 = new Belajar();
+		Scanner input = new Scanner(System.in);
+		long nim_mhs;
+		String nama_mhs;
+		int jumlah_kata_hal;
 	}
 }
