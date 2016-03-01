aefe3d66a0955607fb4d66b74bdb3b4f51e93f74
Dian Aslama <dianaslama6697@gmail.com>
2016-03-01 21:48:52 +0700

    nyicil atribut

--- Latihan-PL-Bab-1/src/Mahasiswa/Mahasiswa.java
+++ Latihan-PL-Bab-1/src/Mahasiswa/Mahasiswa.java
@@ -3,4 +3,12 @@ public class Mahasiswa {
     int nim;
     String namaMhs;
     int jmlhKata;
+    
+    void atribut (String namaMhs, int nim, int jlmhKata){
+        this.namaMhs = namaMhs;
+        this.nim = nim;
+        this.jmlhKata = jlmhKata;
+    }
+    
+    
 }
