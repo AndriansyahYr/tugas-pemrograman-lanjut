2584f97ea0f4ef994b44c5b299a09a6ad7d80990
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-01 20:58:37 +0700

    buat kelas nih

---
+++ Latihan-PL-Bab-1/src/CerpenMahasiswa.java
@@ -0,0 +1,9 @@
+/* 
+Nama  : Firman Budi Safrizal
+Nim	  : 155150201111147
+Kelas : TIF - H
+*/
+
+public class CerpenMahasiswa{
+	
+}
---
+++ Latihan-PL-Bab-1/src/CerpenMahasiswa_main.java
@@ -0,0 +1,11 @@
+/* 
+Nama  : Firman Budi Safrizal
+Nim	  : 155150201111147
+Kelas : TIF - H
+*/
+
+public class CerpenMahasiswa_main{
+	public static void main(String args[]){
+		
+	}
+}
